bf936e50fa8525845d252f9366d16efd8d75f8ce
yao1999 <36053374+yao1999@users.noreply.github.com>
2021-02-25 16:31:59 -0500

    register, login is connect with backend & database

--- college_event_website/Users/views.py
+++ college_event_website/Users/views.py
@@ -1,45 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import generics
 from .models import User
+from django.http import HttpResponseRedirect
 # from .forms import RegisterUser
-from django.http import HttpResponse, HttpResponseRedirect
 
+def check_input(inputs):
+  for input in inputs:
+    if len(input) <= 0:
+      return False
+  
+  return True
 
 def user_register(response):
   if response.method == "POST":
-    if response.POST.get("UserRegisterButton"):
+    if response.POST.get("UserRegisterButton") or response.POST.get("SuperUserRegisterButton"):
       first_name = response.POST.get("UserFirstName")
       last_name = response.POST.get("UserLastName") 
       email = response.POST.get("UserEmail")
       username = response.POST.get("UserUsername")
       password = response.POST.get("UserPassword")
+      is_super_admin = False
+      if response.POST.get("SuperUserRegisterButton"):
+        is_super_admin = True
 
       current_user = User(
-        first_name = first_name,
-        last_name = last_name,
-        email = email,
-        username = username,
-        password = password,
-        is_admin = False,
-        is_super_admin = False
-      )
+          first_name = first_name,
+          last_name = last_name,
+          email = email,
+          username = username,
+          password = password,
+          is_admin = False,
+          is_super_admin = is_super_admin
+        )
       current_user.save()
-      # print(first_name)
-      # print(last_name)
-      # print(email)
-      # print(username)
-      # print(password)
-      
+      return HttpResponseRedirect('../../Users/login')
 
-          
-    # elif response.POST.get()
-    # form = RegisterUser(response.POST)
-    # if form.is_valid() == True:
-      # current_user = User(
-      #   username = form.cleaned_data["username"],
-      #   password = form.cleaned_data["password"],
-      #   email = form.cleaned_data["email"]
-      #   )
+    else:
+      return HttpResponseRedirect('../../Users/register')
+     
 
   return render(response, "Users/register.html") 
 
+
+def user_login(response):
+  if response.method == "POST":
+    if response.POST.get("UserLoginButton") or response.POST.get("SuperUserLoginButton"):
+      username = response.POST.get("UserUsername")
+      password = response.POST.get("UserPassword")
+
+      if response.POST.get("UserLoginButton"):
+        current_user = User.objects.get(username = username, is_super_admin = False)
+      else:
+        current_user = User.objects.get(username = username, is_super_admin = True)
+      
+      if current_user.password == password:
+        return redirect('home')
+
+      else:
+        return HttpResponseRedirect('../../Users/register')
+
+    else:
+      return HttpResponseRedirect('../../Users/login')
+
+  return render(response, "Users/login.html") 
