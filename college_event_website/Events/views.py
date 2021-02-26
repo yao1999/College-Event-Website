ed9f3802b29270c363adb73f0b155ee9d269b326
yao1999 <36053374+yao1999@users.noreply.github.com>
2021-02-25 20:06:25 -0500

    new change

--- college_event_website/Events/views.py
+++ college_event_website/Events/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import traceback 
 # Create your views here.
 
 def event(request):
-  return render(request, 'Events/base.html')
+  try:
+    return render(request, 'Events/base.html')
+  except: 
+    # printing stack trace 
+    traceback.print_exc()
--- college_event_website/Users/views.py
+++ college_event_website/Users/views.py
@@ -2,61 +2,68 @@ from django.shortcuts import render, redirect
 from rest_framework import generics
 from .models import User
 from django.http import HttpResponseRedirect
-# from django.contrib.auth import authenticate
+import traceback 
 from django.contrib.auth import login
 # from .forms import RegisterUser
 
 def user_register(response):
-  if response.method == "POST":
-    if response.POST.get("UserRegisterButton") or response.POST.get("SuperUserRegisterButton"):
+  try:
+    if response.method == "POST":
+      if response.POST.get("UserRegisterButton") or response.POST.get("SuperUserRegisterButton"):
 
-      first_name = response.POST.get("UserFirstName")
-      last_name = response.POST.get("UserLastName") 
-      email = response.POST.get("UserEmail")
-      username = response.POST.get("UserUsername")
-      password = response.POST.get("UserPassword")
+        first_name = response.POST.get("UserFirstName")
+        last_name = response.POST.get("UserLastName") 
+        email = response.POST.get("UserEmail")
+        username = response.POST.get("UserUsername")
+        password = response.POST.get("UserPassword")
+        
+        is_super_admin = False
+        if response.POST.get("SuperUserRegisterButton"):
+          is_super_admin = True
+
+        current_user = User(
+            first_name = first_name,
+            last_name = last_name,
+            email = email,
+            username = username,
+            password = password,
+            is_admin = False,
+            is_super_admin = is_super_admin
+          )
+        current_user.save()
+        return HttpResponseRedirect('../../Users/login')
+
+      else:
+        return HttpResponseRedirect('../../Users/register')
       
-      is_super_admin = False
-      if response.POST.get("SuperUserRegisterButton"):
-        is_super_admin = True
-
-      current_user = User(
-          first_name = first_name,
-          last_name = last_name,
-          email = email,
-          username = username,
-          password = password,
-          is_admin = False,
-          is_super_admin = is_super_admin
-        )
-      current_user.save()
-      return HttpResponseRedirect('../../Users/login')
-
-    else:
-      return HttpResponseRedirect('../../Users/register')
-     
-
-  return render(response, "Users/register.html") 
+    return render(response, "Users/register.html") 
+  except: 
+    # printing stack trace 
+    traceback.print_exc() 
 
 
 def user_login(response):
-  if response.method == "POST":
-    if response.POST.get("UserLoginButton") or response.POST.get("SuperUserLoginButton"):
-      username = response.POST.get("UserUsername")
-      password = response.POST.get("UserPassword")
+  try:
+    if response.method == "POST":
+      if response.POST.get("UserLoginButton") or response.POST.get("SuperUserLoginButton"):
+        username = response.POST.get("UserUsername")
+        password = response.POST.get("UserPassword")
 
-      if response.POST.get("UserLoginButton"):
-        current_user = User.objects.get(username = username, is_super_admin = False)
+        if response.POST.get("UserLoginButton"):
+          current_user = User.objects.get(username = username, is_super_admin = False)
+        else:
+          current_user = User.objects.get(username = username, is_super_admin = True)
+        
+        if current_user.password == password:
+          login(response, current_user)
+          return HttpResponseRedirect('../../Events/')
+        else:
+          return HttpResponseRedirect('../../Users/register')
+
       else:
-        current_user = User.objects.get(username = username, is_super_admin = True)
-      
-      if current_user.password == password:
-        login(response, current_user)
-        return HttpResponseRedirect('../../Events/')
-      else:
-        return HttpResponseRedirect('../../Users/register')
+        return HttpResponseRedirect('../../Users/login')
 
-    else:
-      return HttpResponseRedirect('../../Users/login')
-
-  return render(response, "Users/login.html") 
+    return render(response, "Users/login.html") 
+  except: 
+    # printing stack trace 
+    traceback.print_exc()
