b2e0860753876285ed78580459a51e431f6de292
xartol <xartol.dev@gmail.com>
2021-03-02 16:09:58 -0500

    adds event detail / list pages

--- college_event_website/Events/urls.py
+++ college_event_website/Events/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
    path('create', views.add_event, name='add_event'),
-   path('', views.event, name = 'event')
+   path('', views.event, name = 'event'),
+   path('details', views.event_info, name='event_info')
 ]
