379775fa43bc4bed2501a1df8b492727273de51f
xartol <xartol.dev@gmail.com>
2021-03-02 19:44:40 -0500

    adds event name

--- college_event_website/Events/urls.py
+++ college_event_website/Events/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
    path('create', views.add_event, name='add_event'),
-   path('', views.event, name = 'event'),
+   path('', views.list_events, name = 'list_events'),
    path('details', views.event_info, name='event_info')
 ]
--- college_event_website/Events/views.py
+++ college_event_website/Events/views.py
@@ -1,67 +1,47 @@
 from django.shortcuts import render
 from crispy_forms.helper import FormHelper
 from .forms import EventForm, CommentForm
-import traceback 
 from django.http import HttpResponseRedirect, HttpResponse
 from .models import Event
 
 # Create your views here.
 
-def event(response):
-  try:
-    return render(response, "Events/base.html")
-  except: 
-    # printing stack trace 
-    traceback.print_exc()
+def list_events(response):
+  # return render(response, "Events/base.html")
+  events = Event.objects.all()
+  print(events)
+  return render(response, 'Events/base.html', { 'events' : events})
 
 def add_event(response):
-  try:
-    # model = Event
-    if response.method == "POST":
-      if response.POST.get("create-event-btn"):
-        event_form = EventForm(response.POST)
-        is_private = response.POST.get("universityEvent")
-        is_RSO = response.POST.get("rsoEvent")
-        if event_form.is_valid():
-            event_form.save(is_private, is_RSO)
-        return HttpResponseRedirect('../../Events/')
-      else:
-        return HttpResponseRedirect('../../Events/create')
+  if response.method == "POST":
+    if response.POST.get("create-event-btn"):
+      event_form = EventForm(response.POST)
+      is_private = response.POST.get("universityEvent")
+      is_RSO = response.POST.get("rsoEvent")
+      if event_form.is_valid():
+          event_form.save(is_private, is_RSO)
+      return HttpResponseRedirect('../../Events/')
     else:
-      event_form = EventForm(None)
-      return render(response, "Events/create.html", { 'form' : event_form })
-  except: 
-    # printing stack trace 
-    traceback.print_exc()
+      return HttpResponseRedirect('../../Events/create')
+  event_form = EventForm(None)
+  return render(response, "Events/create.html", { 'form' : event_form })
 
 def delete_event(response):
-  try:
-    if response.method == "POST":
-      pass
-    return render(response, 'Events/?????')
-  except: 
-    # printing stack trace 
-    traceback.print_exc()
+  if response.method == "POST":
+    pass
+  return render(response, 'Events/?????')
 
 def edit_event(response):
-  try:
-    if response.method == "POST":
-      pass
-    return render(response, 'Events/?????')
-  except: 
-    # printing stack trace 
-    traceback.print_exc()
+  if response.method == "POST":
+    pass
+  return render(response, 'Events/?????')
 
 def event_info(response):
-  try:
-    if response.method == "POST":
-      comment_form = CommentForm(response.POST)
-      if comment_form.is_valid():
-          comment_form.save()
-      return render(response, 'Events/details.html', { 'form' : comment_form })
-    else:
-      comment_form = CommentForm(None)
-      return render(response, "Events/details.html", { 'form' : comment_form })
-  except: 
-    # printing stack trace 
-    traceback.print_exc()
+  if response.method == "POST":
+    comment_form = CommentForm(response.POST)
+    if comment_form.is_valid():
+        comment_form.save()
+    return render(response, 'Events/details.html', { 'form' : comment_form })
+  else:
+    comment_form = CommentForm(None)
+    return render(response, "Events/details.html", { 'form' : comment_form })
