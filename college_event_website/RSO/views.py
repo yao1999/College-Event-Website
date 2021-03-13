b3d91cfa80eedd5577bfbf5391a7851f438d13ce
xar-tol <xartol.dev@gmail.com>
2021-03-13 01:08:41 -0500

    adds rso frontend, fixes univ frontend
    adds rso details
    adds rso create
    adds rso list page
    fixed university picture gen

--- college_event_website/RSO/views.py
+++ college_event_website/RSO/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from crispy_forms.helper import FormHelper
+from .forms import RsoForm
+from django.http import HttpResponseRedirect, HttpResponse
+from Users.models import User
+from .models import Rso
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='/Users/login/')
+def list_rsos(response):
+    rsos = Rso.objects.all().order_by('id')
+    return render(response, 'RSO/base.html', {
+      'universities': rsos
+    })
+
+@login_required(login_url='/Users/login/')
+def add_rso(response):
+    RSOForm = RsoForm()
+    return render(response, "RSO/create.html", {
+        'form': RSOForm
+    })
+
+@login_required(login_url='/Users/login/')
+def rso_info(response):
+    return render(response, 'RSO/details.html')
+  
+
+
+def delete_rso():
+    pass
+
+def edit_rso():
+    pass
