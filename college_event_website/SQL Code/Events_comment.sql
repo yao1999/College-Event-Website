abba1da6edd7f3d34673e4dbf1d16156e92828ab
Zheng <jialinzheng96@gamil.com>
2021-04-15 21:45:16 -0400

    sql done

--- college_event_website/SQL Code/Events_comment.sql
+++ college_event_website/SQL Code/Events_comment.sql
@@ -6,3 +6,20 @@ CREATE TABLE Event_comment(
     user_id INT NOTNULL,
     timestamp TIME NOTNULL
 );
+
+INSERT INTO Event_comment(
+    id,	
+    content,	
+    rating,	
+    event_id,	
+    user_id,	
+    timestamp
+)
+VALUES (
+    10,	
+    "you guys are right, this event is not bad",	
+    5,	
+    1,	
+    11,	
+    "2021-04-16 00:00:06"
+)
---
+++ college_event_website/SQL Code/user_rsonumber.sql
@@ -0,0 +1,17 @@
+CREATE TABLE user_rsonumber(
+    id INT PRIMARY KEY NOTNULL,
+    username VARCHAR(100) NOTNULL,
+    rso INT NOT NULL
+);
+
+INSERT INTO university_rsonumber(
+    id, 
+    username,
+    rso
+)
+
+VALUES(
+    1,	
+    Oliver,	
+    2
+) 
