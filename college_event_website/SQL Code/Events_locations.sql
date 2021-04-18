d46287781e8dbd088f3e55bcf22bd82cbd8e3353
Zheng <jialinzheng96@gamil.com>
2021-04-18 15:19:23 -0400

    update

--- college_event_website/SQL Code/Events_locations.sql
+++ college_event_website/SQL Code/Events_locations.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Evetns_locations(
+CREATE TABLE Events_locations(
     id INT PRIMARY KEY NOTNULL,
     location_name VARCHAR(100) NOTNULL,
     latitude VARCHAR(100) NOTNULL,
     longitude VARCHAR(100) NOTNULL
     
 );
-INSERT INTO Evetns_locations(
+INSERT INTO Events_locations(
     id,
     location_name,
     latitude,
